4285ce97f7bc00ba6855032aa8507942dd24e7f3
mohamedEldeeb13 <mooel744@gmail.com>
2024-02-18 18:51:31 +0200

    Initial Commit

---
+++ collectionView-DemoUITests/collectionView_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  collectionView_DemoUITestsLaunchTests.swift
+//  collectionView-DemoUITests
+//
+//  Created by Mohamed Abd Elhakam on 18/02/2024.
+//
+
+import XCTest
+
+final class collectionView_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
